8d783b0d46feb97b7f2a733ca99bd015ff387bae
byAbbadon <kasiopess@abv.bg>
2020-09-29 12:31:55 +0300

    add

---
+++ 12. Lab: Modules/02. Map Sort/app.js
@@ -0,0 +1,14 @@
+let mapSort = require('./mapSort.js')
+
+
+// let map = new Map();
+// map.set(3,"Pesho");
+// map.set(1,"Gosho");
+// map.set(7,"Aleks");
+
+// console.log(mapSort(map));
+//console.log(mapSort(map, sortFn))
+
+result.mapSort = mapSort;
+ 
+//result.mapSort = require('./map-sort');

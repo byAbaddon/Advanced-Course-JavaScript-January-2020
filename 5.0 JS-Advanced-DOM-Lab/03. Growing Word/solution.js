fa0a812ead4961d0803fe7f5b1a8804bdd16192d
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2020-12-25 01:57:35 +0200

    Update solution.js

--- 5.0 JS-Advanced-DOM-Lab/03. Growing Word/solution.js
+++ 5.0 JS-Advanced-DOM-Lab/03. Growing Word/solution.js
@@ -1,27 +1,43 @@
-function growingWord() { //----------------------------(1)-------Fucking Judge Fuck off
+function growingWord() {
+    const colors = ['blue', 'green', 'red'];
 
-  let firstColorElement = $('#colors > div').first()
-  let getColor = firstColorElement.text()
-  $('#colors').append(firstColorElement)
+    const paragraph = document.getElementsByTagName('p')[2];
 
-  let $p = $('#exercise p')
-  $p.css('color', `${getColor}`)
-  $p.css('font-size', '4px')
+    let color = paragraph.style.color;
+    paragraph.style.color = !color ? 'blue' : colors[(colors.indexOf(color) + 1) % colors.length];
 
-  let getNumber = document.querySelector('div.conditions').lastElementChild
-  let text = getNumber.textContent
-  let size = text.match(/\d+/)[0]
-
-   $p.css('font-size', `${+size+ 2}px`)
-
-  getNumber.textContent = +size + 2
-
+    let fontSize = paragraph.style.fontSize.replace('px', '');
+    paragraph.style.fontSize = (!fontSize ? '2' : fontSize * 2) + 'px';
 }
 
 
 
+//----------------------------(2)-----------------------------Fucking Judge
 
-// function growingWord() { //----------------------------(2)-------Fucking Judge Fuck off
+// function growingWord() { 
+
+//   let firstColorElement = $('#colors > div').first()
+//   let getColor = firstColorElement.text()
+//   $('#colors').append(firstColorElement)
+
+//   let $p = $('#exercise p')
+//   $p.css('color', `${getColor}`)
+//   $p.css('font-size', '4px')
+
+//   let getNumber = document.querySelector('div.conditions').lastElementChild
+//   let text = getNumber.textContent
+//   let size = text.match(/\d+/)[0]
+
+//    $p.css('font-size', `${+size+ 2}px`)
+
+//   getNumber.textContent = +size + 2
+
+// }
+
+
+
+
+// function growingWord() { //----------------------------(3)-------Fucking Judge
 
 //   let colorCollection = document.getElementById('colors')
 //   let firstChild = colorCollection.firstElementChild
